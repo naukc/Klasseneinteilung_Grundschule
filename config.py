22bb77be5a765bd430478a6e226d400d3a303f73
Stefan Jordan <sjordan@hm.edu>
2025-08-31 22:21:28 +0200

    added requiremtets.txt

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ PUNKTE_SPRENGEL_GLEICH = 3
 
 # Negative Punkte (Strafen)
 STRAFE_TRENNUNG_MISSACHTET = -1000
-STRAFE_ABWEICHUNG_GESCHLECHT = -5      # pro Schüler Abweichung vom 50/50-Ideal
+STRAFE_ABWEICHUNG_GESCHLECHT = -20      # pro Schüler Abweichung vom 50/50-Ideal
 STRAFE_ABWEICHUNG_MIGRATION = -3       # pro Prozentpunkt Abweichung vom Stufenschnitt
 STRAFE_ABWEICHUNG_AUFFAELLIGKEIT = -2  # pro Punkt Abweichung von der idealen "Belastungssumme"
 
